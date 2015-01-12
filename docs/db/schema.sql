8cd893834f7166c349c2c2c4f9248debacbe9aef
dustin.zhang <vollov.ca@gmail.com>
2015-01-11 21:55:10 -0500

    SPR-1 add db schema.sql

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -24,17 +24,17 @@ CREATE TABLE property (
   city varchar(24) NOT NULL,
   province varchar(2) NOT NULL,
   postcode varchar(10) NOT NULL,
-  type int NOT NULL,
+  type int(2) NOT NULL,
   latitude double,
   longitude double,
   start_price integer,
   active BIT(1),
   owner_id int(11) unsigned NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT property_fk_owner_id FOREIGN KEY (owner_id) REFERENCES user (id),
+  CONSTRAINT property_fk_owner_id FOREIGN KEY (owner_id) REFERENCES user (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO property VALUES
-(1,'24 green valley DR','Kiechener','ON','N2P2J8',1,12,13,1400,1),
-(2,'annie','smith','519-234-6728','annie.smith@gmail.com','pwd',1),
-(3,'luke','bright','519-781-6238','luke.bright@gmail.com','pwd',1);
+(1,'24 green valley DR','Kiechener','ON','N2P2J8',1,12,13,1400,1,1),
+(2,'284 purple sage CR','Kiechener','ON','N2E4G6',1,12,13,1450,0,1),
+(3,'73 amherst DR','Kiechener','ON','N2P1C9',2,12,13,550,0,1);
