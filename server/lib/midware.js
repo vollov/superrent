83e00c8099927391a1c232348052a542c3a53f1e
dike.zhang <dike.zhang@gmail.com>
2015-01-24 23:06:51 -0500

    enable http header

--- server/lib/midware.js
+++ server/lib/midware.js
@@ -7,8 +7,8 @@ module.exports = function(){
 		 * Add Access-Control-Allow-Headers in HTTP response
 		 */
 		header : function(req, res, next) {
-			//res.header('Access-Control-Allow-Origin', '*');
-			//res.header('Access-Control-Allow-Headers', 'Accept,Content-Type, X-Requested-With');
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Headers', 'Accept,Content-Type, X-Requested-With');
 			res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 			//res.header('Access-Control-Allow-Credentials': true);
 			next();
